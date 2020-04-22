71200a3cecc7adee2b89a5cb3ce2d3fc2e8c6534
rrtutors <rrtutor.me@gmail.com>
2020-04-22 15:01:20 +0530

    Initla Push

---
+++ lib/const.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+//final themeColor = Color(0xff15EC70);
+final themeColor = Colors.teal;
+final primaryColor = Color(0xff203152);
+final greyColor = Color(0xffaeaeae);
+final greyColor2 = Color(0xffE8E8E8);
+final chatbox = Color(0xff7D3C98);
+final chatbox2 = Color(0xff7F8C8D);
+final textWhite = Color(0xffFAFAFA);
+final pink = Colors.pink;
